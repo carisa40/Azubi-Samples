2ed9925911a3fcd5370a0c29d18df9ff95e350b2
Anita Blege <carizoe1@gmail.com>
2020-05-15 20:00:13 +0000

    update

--- wages_earned/Python_wage_calculator.py
+++ wages_earned/Python_wage_calculator.py
@@ -1,38 +1,36 @@
 import datetime
 now = datetime.datetime.now()
 
-f= open("hours_worked.txt","a")
-f.write("\n")
-f.write("Start Time,  End Time,  Wages Earned \n")
+"""
+f= open("hours_worked.csv","a")
+
+text = ' , Start Time, End Time, Total Time (hrs), Wages Earned ($), '
+text_f = text.split(",")
+f.write(str(text_f))
+f.write('\n')
 f.close()
+"""
+
 start_time = input("Hi Nana, enter current time to clock-in using 24hr HH.MM format : \n")
 text1 = "New session, You clocked in at : \n"
 time1 = now.strftime("%Y-%m-%d %H:%M:%S")
 print (text1, time1 )
-f= open("hours_worked.txt","a")
-f.write("\n")
-f.write(text1)
-f.write(time1)
-f.write("\n")
-f.close()
 
 end_time = input ("Hi Nana, enter current time to clockout using 24hr HH.MM format : \n")
 text2 = "You clocked out at : \n"
 time2 = now.strftime("%Y-%m-%d %H:%M:%S")
-print (text2, time2)
-f= open("hours_worked.txt","a")
-f.write("\n")
-f.write(text2)
-f.write(time2)
-f.write("\n")
-f.close()
 
+total_time = float(end_time) - float(start_time)
 wages = (float(end_time) - float(start_time)) * 5
-text3 = (("You have earned : " + "${:.2f}" + " this period.").format(wages))
+
+formatted_time = "{:.2f}".format(total_time)
+formatted_wage = "{:.2f}".format(wages)
+text3 = (("You have worked " + "{:.2f} hours " + "and earned : " + "${:.2f}" + " this period.").format(total_time,wages))
 print(text3)
-f= open("hours_worked.txt","a")
-f.write("\n")
-f.write(str(text3))
-f.write("\n")
-f.write("*************************************************************** \n")
+f= open("hours_worked.csv","a")
+
+text4 = [' ', str(time1), str(time2), str(formatted_time), str(formatted_wage), ' ']
+[i.split(',', 1)[0] for i in text4]
+f.write(str(text4))
+f.write('\n')
 f.close()
