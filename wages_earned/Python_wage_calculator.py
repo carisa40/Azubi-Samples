dda8fa7d6e1cdcf9b87a766fed3c82f785aad364
Anita Blege <carizoe1@gmail.com>
2020-05-16 21:20:02 +0000

    change time calc. to system time

--- wages_earned/Python_wage_calculator.py
+++ wages_earned/Python_wage_calculator.py
@@ -1,32 +1,38 @@
 from datetime import datetime
+from datetime import timedelta
+import datetime
 import time
-now = datetime.now()
+now = datetime.datetime.now()
 t = time.localtime()
 """
 f= open("hours_worked.csv","a")
 
-text = ' , Start Time, End Time, Total Time (hrs), Wages Earned ($), '
+text = ' , Start Time, End Time, Total Time (hrs.mins), Wages Earned ($), '
 text_f = text.split(",")
 f.write(str(text_f))
 f.write('\n')
 f.close()
 """
 
-start_time = input("Hi Nana, enter current time to clock-in using 24hr HH.MM format : \n")
+start_time = input("Hi Nana, type \'start\' to clock-in \n")
 text1 = "New session, You clocked in at : \n"
-time1 = now.strftime("%Y-%m-%d %H:%M:%S")
+time1 = now.strficoytime("%Y-%m-%d %H:%M:%S")
 print (text1, time1 )
+#time3 = int(datetime.timedelta(now))
+time3 = now.strftime('%H.%M')
 
 
-end_time = input ("Hi Nana, enter current time to clockout using 24hr HH.MM format : \n")
-text2 = "You clocked out at : \n"
-#time2 = now.strftime("%Y-%m-%d %H:%M:%S")
+
+end_time = input ("Hi Nana, type \'end\' to clock out \n")
+text2 = "End session, You clocked out at : \n"
 #print (text2, time2)
 time2 = time.strftime("%Y-%m-%d %H:%M:%S")
+time4 = time.strftime('%H.%M')
 print(time2)
+#time4 = int(datetime.timedelta(t))
 
-total_time = float(end_time) - float(start_time)
-wages = (float(end_time) - float(start_time)) * 5
+total_time = float(time4 )- float(time3)
+wages = (total_time) * 5
 
 formatted_time = "{:.2f}".format(total_time)
 formatted_wage = "{:.2f}".format(wages)
